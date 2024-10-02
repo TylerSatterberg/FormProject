41390aa4e273076a5fda8728aed4acf3e7904514
tylersatterberg <satterbergtyler@gmail.com>
2024-10-02 13:55:20 -0700

    finished form

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -6,17 +6,36 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/p1")
-def render_page1():
-    return render_template('page1.html')
+@app.route("/resp")
+def render_resp():
+    name1 = request.args['name1']
+    name2 = request.args['name2']
+    genre = request.args['genre']
+    tennis = request.args['tennis']
+    Solo = request.args['Solo']
+    reply1 = "hello " + name1, " " + name2
+    if genre == 'option1':
+        reply2 = "Thats my favorite genre too, I like Minecraft!"
+    else:
+        reply2 = "I prefer sandbox games, like Minecraft"
+    if tennis == 'option4':
+        reply3 = "You are correct, Lucy would completely destory Tyler in a 1v1"
+    else:
+        reply3 = "Sorry but you are wrong, Tyler is no where near as good as Lucy if they played in a 1v1"
+    if Solo == 'option10':
+        reply4 = "Correct! Kenji fish slice is the only way to win solo showdown as long as you dont miss"
+    if Solo == 'option9':
+        reply4 = "Correct, Kit boxing is a guarenteed way to win solo showdown!"
+    if Solo == 'option8':
+        reply4 = "Correct, Edgar jump can kill anyone"
+    if Solo == 'option7':
+        reply4 = "No, unless you are Frank this is an absolute garbage and unwinable way to play solo showdown D:"
+    if Solo == 'option6':
+        reply4 = "You are a villain, no one likes you, delete the game you trashcan"
+    return render_template('resp.html', r1 = reply1, r2 = reply2, r3 = reply3, r4 = reply4,  name1=name1, name2=name2, genre=genre, tennis=tennis, Solo=Solo)
 
-@app.route("/p2")
-def render_page2():
-    return render_template('page2.html')
-
-@app.route("/p3")
-def render_page3():
-    return render_template('page3.html')
-    
 if __name__=="__main__":
     app.run(debug=True)
+
+
+#the fitnessgram pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues
